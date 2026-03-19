c188a50f01b481ffdc896980bb1b114dbcdea0d9
Henrik Asplund <232446152+henkkasplund@users.noreply.github.com>
2026-03-19 12:00:31 +0200

    Submit 1.14:anecdotes step3

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -8,11 +8,13 @@ const Statistics = ({stats}) => {
 const Anecdote = ({anecdote, stats}) => {
   return (
     <div>
-      <p>{anecdote}</p>
+      {anecdote}
       <Statistics stats={stats} />
     </div>
   )
 }
+
+const Header = ({text}) => <h1>{text}</h1>
 
 const Button = ({onClick, name}) => <button onClick={onClick}>{name}</button>
 
@@ -27,25 +29,33 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
     'The only way to go fast, is to go well.'
   ]
-  
   const [selected, setSelected] = useState(0)
   const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
-  const updateSelected = (anecdotes) => Math.floor(Math.random() * anecdotes.length)
+  const mostVotes = votes.indexOf(Math.max(...votes))
+  const updateSelected = (anecdotes) => {
+    let next = selected
+    while (next == selected) {
+      next = Math.floor(Math.random() * anecdotes.length)
+    }
+    return next
+  }
   const handleAnecdote = () => {
     setSelected(updateSelected(anecdotes))
   }
   const handleVote = () => {
     const updatedVotes = [...votes]
     updatedVotes[selected] += 1
-    console.log(selected)
     setVotes(updatedVotes)
   }
 
   return (
     <div>
+      <Header text="Anecdote of the day" />
       <Anecdote anecdote={anecdotes[selected]} stats={votes[selected]} />
       <Button onClick={handleVote} name='vote' />
       <Button onClick={handleAnecdote} name='next anecdote' />
+      <Header text="Anecdote with most votes" />
+      <Anecdote anecdote={anecdotes[mostVotes]} stats={votes[mostVotes]} />
     </div>
   )
 }
