7ba3ef0f0257584afbd977326a49d548ef5f2989
Henrik Asplund <232446152+henkkasplund@users.noreply.github.com>
2026-03-22 22:52:40 +0200

    Submit 2.9: Phonebook Step 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,62 +1,81 @@
 import { useState } from 'react'
 
-const Contact = ({ name, number }) => <div>{ name } { number }</div>
+const FilterContacts = ({ contacts, criteria }) => {
+  return (
+    criteria === '' ? <Contacts contacts={ contacts } /> : <Contacts contacts={contacts.filter((contact) => 
+      contact.name.toLowerCase().includes(criteria.toLowerCase()))} />
+  )
+}
+
+const Contact = ({name, number }) => {
+  return (
+    <tr>
+      <td>{ name }</td>
+      <td>{ number }</td>
+    </tr>
+  )
+}
+
+const Contacts = ({ contacts }) => {
+  return (
+    <table>
+      <tbody>
+        {contacts.map((contact) => <Contact key={ contact.id } name={ contact.name } number={ contact.number } /> )}
+      </tbody>
+    </table>
+  )
+}
 
 const App = () => {
   const [contacts, setContacts] = useState([
-    { id: 1, name: 'Arto Hellas', number: '040-3798290', important: true}
-  ]) 
+    { id: 1, name: 'Arto Hellas', number: '0403902920' }
+]) 
   const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber ] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [searchCriteria, setSearchCriteria] = useState('')
 
   const addContact = (event) => {
     event.preventDefault()
-    if (contacts.some((contact) => contact.name === newName)) {
-      alert(`${ newName } is already added to phonebook`)
-      setNewName('')
-      return
-    }
     if (newName === '' || newNumber === '') {
       alert('Both name and number have to be added')
+      return
+    }
+    if (contacts.some((contact) => contact.name === newName)) {
+      alert(`${newName} has already been added to phonebook`)
+      setNewName('')
       return
     }
     const contactObject = {
       id: contacts.length +1,
       name: newName,
       number: newNumber,
-      important: Math.random() > 0.5
     }
     setContacts(contacts.concat(contactObject))
     setNewName('')
     setNewNumber('')
   }
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
-
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
-
   return (
     <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={ addContact }>
+      <h1>Phonebook</h1>
+      <h2>Add new contact</h2>
+      <form onSubmit={ addContact } >
         <div>
-          name: <input value={ newName } onChange={ handleNameChange } />
+          name: <input value={ newName } onChange={(event) => setNewName(event.target.value)} />
         </div>
         <div>
-          number: <input value={ newNumber } onChange={ handleNumberChange } />
+          number: <input value={ newNumber } onChange={(event) => setNewNumber(event.target.value)} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
+      <h2>Search</h2>
       <div>
-        {contacts.map((contact) => <Contact key={ contact.id } name={ contact.name } number={ contact.number } />)}
+        filter by name: <input value={ searchCriteria } onChange={(event) => setSearchCriteria(event.target.value)} />
       </div>
+      <h2>Contacts</h2>
+        <FilterContacts contacts={ contacts } criteria={ searchCriteria } />
     </div>
   )
 }
