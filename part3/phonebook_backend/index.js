fc5e8c0a5d9c2ba20e7b02f558a9954889216c0f
Henrik Asplund <232446152+henkkasplund@users.noreply.github.com>
2026-03-31 12:59:44 +0300

    Submit 3.5: Phonebook backend step 5

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,14 +1,10 @@
 const express = require('express')
-let persons = require('./persons')
+const { persons, addPerson, deletePerson } = require('./persons')
 const info = require('./info')
 
 const app = express()
 
 app.use(express.json())
-
-app.get('/', (request, response) => {
-  response.send('<h1>Phonebook</h1>')
-})
 
 app.get('/api/persons/:id', (request, response) => {
   const id = request.params.id
@@ -27,8 +23,23 @@ app.get('/api/info', (request, response) => {
 app.delete('/api/persons/:id', (request, response) => {
   const id = request.params.id
   const person = persons.find((person) => person.id === id)
-  persons = persons.filter((person) => person.id !== id)
+  deletePerson(id)
   person ? response.status(204).end() : response.status(404).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const { name, number} = request.body
+  if (!name && !number) {
+    return response.status(400).json({ error: 'Neither name nor number given' })
+  }
+  if (!name) {
+    return response.status(400).json({ error: 'No name given'})
+  }
+  if (!number) {
+    return response.status(400).json({ error: 'No number given' }) 
+  }
+  const person = addPerson(name, number)
+  response.json(person)
 })
 
 const PORT = 3001
--- part3/phonebook_backend/persons.js
+++ part3/phonebook_backend/persons.js
@@ -1,24 +1,41 @@
-const persons = [
+let persons = [
   { 
     "id": "1",
-    "name": "Arto Hellas", 
+    "name": "Arto Hellas",
     "number": "040-123456"
   },
   { 
     "id": "2",
-    "name": "Ada Lovelace", 
+    "name": "Ada Lovelace",
     "number": "39-44-5323523"
   },
   { 
     "id": "3",
-    "name": "Dan Abramov", 
+    "name": "Dan Abramov",
     "number": "12-43-234345"
   },
   { 
     "id": "4",
-    "name": "Mary Poppendieck", 
+    "name": "Mary Poppendieck",
     "number": "39-23-6423122"
   }
 ]
 
-module.exports = persons
+const generateId = () => String(Math.floor(Math.random() * 10**6))
+
+const addPerson = (name, number) => {
+  const person = {
+    "id": generateId(),
+    "name": name,
+    "number": number
+  }
+  persons.push(person)
+  return person
+}
+
+const deletePerson = (id) => {
+  const index = persons.findIndex((person) => person.id === id)
+  persons.splice(index, 1)
+}
+
+module.exports = { persons, addPerson, deletePerson }
