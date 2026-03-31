6f9f84281095d0c9889254927bb70402a7f3115a
Henrik Asplund <232446152+henkkasplund@users.noreply.github.com>
2026-03-31 14:24:50 +0300

    Submit 3.7: Phonebook backend step 7

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
 const { persons, addPerson, deletePerson } = require('./persons')
 const info = require('./info')
 
 const app = express()
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 app.get('/api/persons/:id', (request, response) => {
   const id = request.params.id
